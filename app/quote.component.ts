76acda04d7f69b541c22d8f7ceea4140db4ba1ea
Unknown <mlink3.14@gmail.com>
2016-09-25 20:43:57 -0600

    API Services (continued)
    Added bitcoin value service routine.
    TODO: encapsulate bitcoin service routine into an @Injectable service

--- app/quote.component.ts
+++ app/quote.component.ts
@@ -7,15 +7,46 @@ import { Quote } from './quote';
 
 @Component({
     selector: 'my-quote',
-    template: '<div><h2>{{quote.quote}}</h2><h3>~{{quote.author}}</h3></div>'
+    template: '<div><h2>{{quote.quote}}</h2><h3>~{{quote.author}}</h3><h3>BC: {{this.bcValue.to_quantity}}</h3></div>'
 })
 export class QuoteComponent {
 
     private quote : Quote = new Quote('','','');
     private requestUrl : string;
     private headerString : string;
+    private bcValue = {};
 
     constructor(private http: Http) {
+
+        this.getRandomQuote();
+        this.getBitcoinValue();        
+    }
+
+    private getBitcoinValue() {
+
+        this.requestUrl = 'https://bravenewcoin-v1.p.mashape.com/convert?from=btc&qty=1&to=usd';
+
+        var localHeaders = new Headers({     
+            
+            'X-Mashape-Key' : 'AXcYZwz5QLmshkiUwJauIltVntULp1LmwjJjsnSQjDK1xB15p7',
+            'Accept' : 'application/json'
+
+        }); 
+
+        var requestOptions = new RequestOptions({
+            method : RequestMethod.Post,
+            url: this.requestUrl,
+            headers: localHeaders
+        });
+
+
+        let response = this.http.request(this.requestUrl, requestOptions).
+        map(this.mapResponse).
+        catch(this.handleError).
+        subscribe(value => this.bcValue = value);
+    }
+
+    private getRandomQuote() {
 
         this.requestUrl = 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous';
 
