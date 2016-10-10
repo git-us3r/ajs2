729f6bed81332d48b58ab2b0884f7bc48f9bb8b8
Unknown <mlink3.14@gmail.com>
2016-10-09 23:08:30 -0600

    Broken ...
    failed to animate ... todo: try ngClass

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,12 +1,12 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpModule }    from '@angular/http';
-import { AppComponent }  from './app.component';
-import { QuoteComponent } from './quote.component';
-import { BTCValueComponent } from './btcvalue.component';
-import { MyIpComponent } from './my-ip.component';
-import { NewsComponent } from './news.component';
-import { ApiService } from './api.service';
+import { NgModule }             from '@angular/core';
+import { BrowserModule }        from '@angular/platform-browser';
+import { HttpModule }           from '@angular/http';
+import { AppComponent }         from './app.component';
+import { QuoteComponent }       from './quote.component';
+import { BTCValueComponent }    from './btcvalue.component';
+import { MyIpComponent }        from './my-ip.component';
+import { NewsComponent }        from './news.component';
+import { ApiService }           from './api.service';
 
 @NgModule({
   imports: [ BrowserModule, HttpModule ],
--- app/news.component.ts
+++ app/news.component.ts
@@ -1,19 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, NgModule, animate, state, style, transition, trigger} from '@angular/core';
 import { OnInit } from '@angular/core';
 import { ApiService } from './api.service';
 
+
 @Component({
     selector: 'news',
-    templateUrl: 'app/news.component.html'
+    templateUrl: 'app/news.component.html',
+    styleUrls: ['app/news.component.css'],
+    animations: [trigger(
+      'openClose',
+      [
+        state('collapsed, void', style({opacity: '0'})),
+        state('expanded', style({opacity: '1'})),
+        transition(
+            'collapsed <=> expanded', [animate(500)])
+      ])],
 })
 export class NewsComponent implements OnInit {
 
     private categoryCollection : any;
-
-
+    private stateExpression: string;  
+     
     constructor(private apiService: ApiService) {
 
-        // EMPTY
+        this._Collapse();
     }
 
     ngOnInit() : void {
@@ -23,9 +33,23 @@ export class NewsComponent implements OnInit {
         subscribe(news => this._HandleCategoriesResponse(news));
     }
 
+    private _Expand() { 
+
+        this.stateExpression = 'expanded'; 
+        console.log("this.stateExpression: " + this.stateExpression);
+    }
+
+    private _Collapse() { this.stateExpression = 'collapsed'; }
+
     private _HandleCategoriesResponse(news : any) : void {
 
         this.categoryCollection = news.response.results;
+        window.setTimeout(this._Expand, 6000);
+    }
+
+    private _NavigateToUrl(url : string) : void {
+
+        window.open(url);
     }
 
 }
