ae4b71a55bcd8dd8f8cb549265ad024f86d1d180
Unknown <mlink3.14@gmail.com>
2016-10-23 14:18:13 -0600

    Messing with animaions ...

--- app/news.component.ts
+++ app/news.component.ts
@@ -1,6 +1,7 @@
 import { Component, NgModule, animate, state, style, transition, trigger} from '@angular/core';
 import { OnInit } from '@angular/core';
 import { ApiService } from './api.service';
+import { Observable } from 'rxjs/Observable';
 
 
 @Component({
@@ -14,10 +15,10 @@ import { ApiService } from './api.service';
             opacity: 0,
             transform: 'translateX(-100%)'
           }),
-          animate('0.2s ease-in')
+          animate('.5s 250ms ease-in')
         ]),
         transition('* => void', [
-          animate('0.2s 10 ease-out', style({
+          animate('.5s 250ms ease-out', style({
             opacity: 0,
             transform: 'translateX(100%)'
           }))
@@ -30,22 +31,35 @@ export class NewsComponent implements OnInit {
     private categoryCollection : any[];
     private displayCategoryCollection : any[];
     private stateExpression: string;  
+    private indexOfLastDisplayedArticle : number;
      
     constructor(private apiService: ApiService) {
 
+        this.indexOfLastDisplayedArticle = 0;
     }
 
-    ngOnInit() : void {
+    public ngOnInit() : void {
 
         let news : any;
         this.apiService.GetGuardianNewsByCategory().
         subscribe(news => this._HandleCategoriesResponse(news));
     }
 
-    private _Expand() { 
+    public _CloseNewsArticle(article : any) {
 
-        this.stateExpression = 'expanded'; 
-        console.log("this.stateExpression: " + this.stateExpression);
+        for(var id in this.displayCategoryCollection) {
+
+            if(this.displayCategoryCollection[id] === article) {
+
+                this.displayCategoryCollection[id].isVisible = false;
+                break;
+            }
+        }
+
+        let newArticle = this.categoryCollection[++this.indexOfLastDisplayedArticle];
+        newArticle.isVisible = true;
+
+        this.displayCategoryCollection.push(newArticle);
     }
 
     private _Collapse() { this.stateExpression = 'collapsed'; }
@@ -55,14 +69,19 @@ export class NewsComponent implements OnInit {
         this.categoryCollection = news.response.results;
         this.displayCategoryCollection = [];
 
+        this._UpdateDisplayCollection();
+    }
+
+    private _UpdateDisplayCollection() : void {
+
         for(var id in this.categoryCollection.slice(0,5)) {
 
             var element = this.categoryCollection[id];
+            element.isVisible = true;
             this.displayCategoryCollection.push(element);
-            console.log("pushed news category: " + element.webTitle);
+        }
 
-            setTimeout(function(_id = id){ console.log("waiting on: " + _id); }, 2000);     
-        }   
+        this.indexOfLastDisplayedArticle = 4;
     }
 
     private _NavigateToUrl(url : string) : void {
@@ -70,4 +89,13 @@ export class NewsComponent implements OnInit {
         window.open(url);
     }
 
+    private _Sleep(ms : number) {
+
+        let entryTime = Date.now();
+
+        while(Date.now() <= entryTime + ms) {
+
+            // spin
+        }
+    }
 }
